d4ec2687dfa91d46340d9415f0b7e97e72add466
Ashish Prajapati <ashisherc@gmail.com>
2018-07-27 12:31:09 +0530

    return user after registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,13 +17,13 @@ router.post('/register', bodyValidator(userRegisterSchema), async (req, res, nex
         user.password = passwordHash;
         await authRepository.createUser(user);
 
-        const userDetails = await authRepository.getUser(user.email);
+        var userDetails = await authRepository.getUser(user.email);
         var accessToken = await getAccessToken(userDetails);
     } catch (error) {
         next(error);
         return;
     }
-    res.send({ accessToken });
+    res.send({ accessToken, user: userDetails });
 });
 
 router.post('/login', bodyValidator(userLoginSchema), async (req, res, next) => {
