f8d6d6984edb680a27aefbebd2f1914d880b3cc9
Fredrik Engberg <fredrik@everlife.ai>
2018-09-26 13:52:05 +0200

    Moves source files into `src` separate them from the `node_modules` in the IDE.

--- src/repository/accountRepository.js
+++ src/repository/accountRepository.js
@@ -1,4 +1,4 @@
-const { User } = require('everlife-token-sale-model');
+const { User } = require('../../../everlife-token-sale-model/src/index');
 
 /*      outcome/
  * Save a new purchase into the user object.
