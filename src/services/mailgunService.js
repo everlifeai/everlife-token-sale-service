da513e58550cc41fbdd36933f32ddde930e28fa4
Fredrik Engberg <fredrik@everlife.ai>
2018-10-13 14:16:38 +0200

    - Fixed path resolution for mail template. - Added logging.

--- src/services/mailgunService.js
+++ src/services/mailgunService.js
@@ -1,36 +1,41 @@
+const log = require('util').log;
+const path = require('path');
 const emailConfig = require('../config/config');
 const mailgun = require('mailgun-js')(emailConfig.mailgun);
 const pug = require('pug');
 
 module.exports.sendEmail = (recipient, message, attachment) =>
-  new Promise((resolve, reject) => {
+    new Promise((resolve, reject) => {
 
-    // Compile the pub Template
-    const compiledFunction = pug.compileFile(message.assetname);
-    // Render a set of data
+        // Find asset path relative to current file.
+        const templatePath = path.join(__dirname, '../..', message.assetname);
+        log(`[sendEmail] template path: ${templatePath}`);
 
-    const pugMessage = compiledFunction({
-      name: recipient.name
+        // Compile the pub Template
+        const compiledFunction = pug.compileFile(templatePath);
+        // Render a set of data
+
+        const pugMessage = compiledFunction({
+            name: recipient.name
+        });
+
+        log(`[sendEmail] subject: ${message.subject}`);
+
+
+        const data = {
+            from: message.from,
+            to: recipient.email,
+            subject: message.subject,
+            //text: "",
+            inline: attachment,
+            html: pugMessage,
+        };
+
+        mailgun.messages().send(data, function (error, body) {
+            if (error) {
+                log(`[sendEmail] Failed to send email: ${error} data:\n`, data);
+
+            }
+            console.log(body);
+        });
     });
-
-    console.log("subject: "+message.subject);
-
-
-    const data = {
-      from: message.from,
-      to: recipient.email,
-      subject: message.subject,
-      //text: "",
-      inline: attachment,
-      html: pugMessage,
-    };
-
-    mailgun.messages().send(data, function (error, body) {
-      if (error) {
-        //return reject(error);
-        console.log("Error Sending Email", data);
-
-      }
-      console.log(body);
-    });
-  });
